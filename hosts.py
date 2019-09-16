c99b9ba9b102205b9e61a17a8e9cccd5f6423f03
王兴民 <wangxingmin@wecash.net>
2019-09-16 11:31:47 +0800

    fix

--- hosts.py
+++ hosts.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env  python
-#coding:utf-8
+#!/usr/bin/env python
+# encoding: utf-8
 
-import sys
+"""
+@author: gongxiude
+@contact: gongxd@wecash.net
+@file: hosts.py 
+@time: 2019/9/16 11:10 AM
+@description:
+"""
+
 import json
-import os
-import  argparse
+import argparse
+
 try:
-    import requests
-except ImportError as e:
-    print(e.args[0])
+    from urllib.request import urlopen, Request  # Python 3
+except ImportError:
+    from urllib2 import urlopen, Request  # Python 2
 
-
-# environment = os.getenv("environment")
-
-# if environment is None:
-#     raise Exception("没有从系统中获取到environment变量， 请执行export environment=prod|test(001~005) 指定环境")
-
-# host = "http://infra.spacebox.fun"
-host = "http://127.0.0.1:8000"
+host = "http://wefocus.svc.wecash.net"
 
 ansible_ssh_user = "root"
 
@@ -26,23 +26,37 @@ class Hosts(object):
     """
     动态获取ansible Inventory
     """
-    def __init__(self, host, timeout=5):
+
+    def __init__(self, host):
         """
-        :param timeout： 调用接口超时时间
+        :param host： 获取ansible资源的接口
         """
 
-        # self.environment = environment
         self.host = host.rstrip("/")
-        self.timeout = timeout
-
-        # if not self.host.startswith("https://"):
-        #     self.host = "https://" + self.host
 
         self.api_url = self.host + "/api/ansible"
 
+    def _do_request(self, url, data=None, err_msg="Error", timeout=5):
+        try:
+            resp = urlopen(
+                Request(url, data=data, headers={"Content-Type": "application/json", "User-Agent": "wefocus"}),
+                timeout=timeout
+            )
+            resp_data, code, headers = resp.read().decode("utf8"), resp.getcode(), resp.headers
+            resp_data = json.loads(resp_data)
+        except Exception as e:
+            raise e
+
+        if code not in [200, 201, 204]:
+            raise ValueError(
+                "{0}:\nUrl: {1}\nData: {2}\nResponse Code: {3}\nResponse: {4}".format(err_msg, url, data, code,
+                                                                                      resp_data))
+        return resp_data, code, headers
+
     def data(self):
+
         d = self.get_hosts_list()
-        
+
         return self.json_format_dict(data=d, pretty=True)
 
     def get_hosts_list(self):
@@ -51,32 +65,29 @@ class Hosts(object):
         environment： 环境名称
         :return: 组ID列表
         """
-        ret = None
-        # data = {"environment": environment}
-        data = {}
-        request = requests.get("{0}".format(self.api_url),params=data, timeout=self.timeout)
-        if request.status_code == 200:
-            ret = request.json()
-            return ret
-        else:
-            raise Exception("获取接口数据失败， 状态码： %s" %(request.status_code))
+        resp_data, _, _ = self._do_request(self.api_url)
+        return resp_data
 
 
     def json_format_dict(self, data, pretty=False):
-        ''' Converts a dict to a JSON object and dumps it as a formatted
-        string '''
+        """
+        格式化输出json
+        :param data:
+        :param pretty:
+        :return:
+        """
         if pretty:
-            return json.dumps(data, sort_keys=True, indent=4, separators=(',',':'))
+            return json.dumps(data, sort_keys=True, indent=4, separators=(',', ':'))
         else:
             return json.dumps(data)
 
     def main(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--list', action = 'store_true')
-        parser.add_argument('--host', action = 'store')
+        parser.add_argument('--list', action='store_true')
+        parser.add_argument('--host', action='store')
         self.args = parser.parse_args()
+
 
 if __name__ == "__main__":
     g = Hosts(host=host)
     print(g.data())
-    # print(json.dumps(da, indent=4, separators=(',',':'))
